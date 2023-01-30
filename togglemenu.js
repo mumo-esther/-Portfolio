502abd39a7727b33ce70937d8951944275599885
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-01-30 16:23:02 +0300

    set up eslint

---
+++ togglemenu.js
@@ -0,0 +1,19 @@
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+openNav();
+closeNav();
+
+const navLinks = document.querySelectorAll('.overlay-item');
+const navMenu = document.getElementById('.overlay-content');
+
+function linkshandler() {
+  navMenu.classList.remove('show');
+}
+navLinks.forEach((n) => n.addEventListener('click', linkshandler));
+navLinks.forEach((n) => n.addEventListener('click', linkshandler));
