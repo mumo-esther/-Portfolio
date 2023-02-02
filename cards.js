d90c91abb2d5d4591d24a8bf4b22e6bc9ae47b02
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-02-02 13:16:20 +0300

    added objects for projects data

--- cards.js
+++ cards.js
@@ -1,12 +1,102 @@
-const works = document.getElementById('works');
+const cards = [
+  {
+    id: 1,
+    title:'Multi-post Stories',
+    h3: 'Gain + Glory',
+    technologies: 
+      {
+    react:'React', 
+    css: 'Css', 
+    javscript: 'JavaScript', 
+    html: 'Html'
+  },
+ description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+ livelink: 'https://app-pokemon-webapp.netlify.app/',
+ seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
+},
 
-const imgArray = ['desktopSnapshoot', 'mobileSnapshoot']
-const titleArray = ['Multi-Post Stories']
-const h3Array = ['Gain + Glory']
-const langArray = ['React', 'Css', 'JavaScript', 'Html']
-const descriptionArray = ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea']
-const demoArray = ['https://app-pokemon-webapp.netlify.app/']
-const sourceArray = ['https://github.com/mavericks-db/capstone03']
+{
+  id: 2,
+  title:'Multi-post Stories',
+  h3: 'Gain + Glory',
+  technologies: 
+    {
+  react:'React', 
+  css: 'Css', 
+  javscript: 'JavaScript', 
+  html: 'Html'
+},
+description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+livelink: 'https://app-pokemon-webapp.netlify.app/',
+seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
+},
+
+{
+  id: 3,
+  title:'Multi-post Stories',
+  h3: 'Gain + Glory',
+  technologies: 
+    {
+  react:'React', 
+  css: 'Css', 
+  javscript: 'JavaScript', 
+  html: 'Html'
+},
+description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+livelink: 'https://app-pokemon-webapp.netlify.app/',
+seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
+},
+
+{
+  id: 4,
+  title:'Multi-post Stories',
+  h3: 'Gain + Glory',
+  technologies: 
+    {
+  react:'React', 
+  css: 'Css', 
+  javscript: 'JavaScript', 
+  html: 'Html'
+},
+description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+livelink: 'https://app-pokemon-webapp.netlify.app/',
+seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
+},
+
+{
+  id: 5,
+  title:'Multi-post Stories',
+  h3: 'Gain + Glory',
+  technologies: 
+    {
+  react:'React', 
+  css: 'Css', 
+  javscript: 'JavaScript', 
+  html: 'Html'
+},
+description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+livelink: 'https://app-pokemon-webapp.netlify.app/',
+seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
+},
+
+{
+  id: 6,
+  title:'Multi-post Stories',
+  h3: 'Gain + Glory',
+  technologies: 
+    {
+  react:'React', 
+  css: 'Css', 
+  javscript: 'JavaScript', 
+  html: 'Html'
+},
+description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+livelink: 'https://app-pokemon-webapp.netlify.app/',
+seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
+}
+]
+
+const works = document.getElementById('works');
 
 for (let i = 0; i < 6; i++) {
   const div = document.createElement('div');
@@ -94,58 +184,3 @@ popbtn.forEach((n) => {
     });
   });
 });
-// mobile popup
-// const mobiletags = ['Ruby on Rails', 'Css', 'Javascript'];
-
-// const mobilepopbtn = document.querySelectorAll('.btn-popup');
-// mobilepopbtn.forEach((n) => {
-//   n.addEventListener('click', () => {
-//     const main = document.createElement('div');
-//     main.className = 'main';
-//     const mobilepopup = document.createElement('div');
-//     mobilepopup.className = 'mobilepopup';
-//     mobilepopup.innerHTML = `<div class="popupclose-btn" id="popupclose-btn">
-//     <button class="close" >
-//                 <i class=" fa fa-times"></i>
-//                 </button>
-//                 </div>
-//                 <div>
-//                   <img src="./images/Snapshoot Portfolio mobile.png" alt="pop-up window image"
-//                    class="mobilepopupcard-image">
-//                 </div>
-//                 <h2 class="card-title">
-//                 ${popupH2[0][0]}
-//                   <br>
-//                    ${popupH2[0][1]}
-//              </h2>
-//                 <ul class="popupcard-tags">
-//                     <li>${mobiletags[0]}</li>
-//                     <li>${mobiletags[1]}</li>
-//                     <li>${mobiletags[2]}</li>
-//                 </ul>
-//                 <h6 class="card-text">
-//                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
-//                 </h6>
-//                 <div class="titleright">
-//                 <a href="">
-//                 <button class="popupbtns" type="button">
-//                 See Live<img src="./images/seelive.png" alt="live icon">
-//                 </button>
-//                 </a>
-//                 <a href="">
-//                 <button class="popupbtns" type="button">
-//                 See Source <img src="./images/white-github.png" alt="github icon">
-//                 </button>
-//                 </a>
-//                 </div>
-//             `;
-//     main.appendChild(mobilepopup);
-//     works.appendChild(main);
-//     const close = document.querySelector('.close');
-//     close.addEventListener('click', () => {
-//       works.removeChild(main);
-//     });
-//   });
-// })
-// ;
- 
