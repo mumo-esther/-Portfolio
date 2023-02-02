044289bb91eab199f4183c4be881ac715413e89e
alexiscyber14 <142sledgehammer14@gmail.com>
2023-02-02 12:59:39 +0100

    Add list links

--- cards.js
+++ cards.js
@@ -95,6 +95,7 @@ seeproject:'see project',
   javscript: 'JavaScript', 
   html: 'Html'
 },
+modalTitle:'Keeping track of hundreds of components',
 description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it 1960s with the relea',
 livelink: 'https://app-pokemon-webapp.netlify.app/',
 seesourcelink: 'https://app-pokemon-webapp.netlify.app/',
@@ -184,6 +185,10 @@ cards.forEach((card) => {
 
   let modalh3 =document.createElement('h3')
   modelContent.appendChild(modalh3)
+  let modalh3Text = document.createTextNode()
+  modalh3.appendChild(modalh3Text)
+
+
 
   let modalUl = document.createElement('ul')
   cardUl.className='card-tags'
@@ -195,13 +200,13 @@ cards.forEach((card) => {
 /* modal links*/
 let modalliOne = document.createElement('li')
 modalUl.appendChild(modalliOne)
-let modalliOneText = document.createTextNode(card.technologies.react)
+let modalliOneText = document.createTextNode(card.technologies.codekit)
 modalliOne.appendChild(modalliOneText)
 
 
 let modalliTwo = document.createElement('li')
 modalUl.appendChild(modalliTwo)
-let modalliTwoText = document.createTextNode(card.technologies.css)
+let modalliTwoText = document.createTextNode(card.technologies.github)
 modalliTwo.appendChild(modalliTwoText)
 
 let modalliThree = document.createElement('li')
@@ -211,135 +216,19 @@ modalliThree.appendChild(modalliThreeText)
 
 let modalliFour = document.createElement('li')
 modalUl.appendChild(modalliFour)
-let modalliFourText = document.createTextNode(card.technologies.html)
+let modalliFourText = document.createTextNode(card.technologies.bootstrap)
 modalliFour.appendChild(modalliFourText)
 
 let modalliFive = document.createElement('li')
 modalUl.appendChild(modalliFive)
-let modalliFiveText = document.createTextNode(card.technologies.react)
+let modalliFiveText = document.createTextNode(card.technologies.terminal)
 modalliFive.appendChild(modalliFiveText)
 
 
 let modalliSix = document.createElement('li')
 modalUl.appendChild(modalliSix)
-let modalliSixText = document.createTextNode(card.technologies.css)
+let modalliSixText = document.createTextNode(card.technologies.codepen)
 modalliSix.appendChild(modalliSixText)
 
 
-
-
-  /*div.innerHTML = `
-  <article class="card-work">
-      <div class="card-image">
-      </div>
-      <h3 class="card-title">
-      ${card.title}
-      </h3>
-      <h3>
-          ${h3Array[0]}
-      </h3>
-      <ul class="card-tags">
-          <li>${langArray[0]}</li>
-          <li>${langArray[1]}</li>
-          <li>${langArray[2]}</li>
-          <li>${langArray[3]}</li>
-      </ul>
-      <button type="button" class="btn-popup">
-          See Project
-      </button>
-  </article>`;*/
-
-
-
-
 });
-
-
-/*
-
-for (let i = 0; i < 6; i++) {
-  const div = document.createElement('div');
-  works.appendChild(div);
-  div.innerHTML = `
-            <article class="card-work">
-                <div class="card-image">
-                </div>
-                <h3 class="card-title">
-                 ${titleArray[0]}
-                </h3>
-                <h3>
-                    ${h3Array[0]}
-                </h3>
-                <ul class="card-tags">
-                    <li>${langArray[0]}</li>
-                    <li>${langArray[1]}</li>
-                    <li>${langArray[2]}</li>
-                    <li>${langArray[3]}</li>
-                </ul>
-                <button type="button" class="btn-popup">
-                    See Project
-                </button>
-            </article>`;
-}
-
-const popupImage = ['desktoppopup', 'mobilepopup']
-const popupH2 = [['Keeping track of hundreds of', 'components']]
-const popupTags = ['Codekit','Github','JavaScript', 'Bootsrap', 'Codepen', 'Terminal']
-// desktop popup
-
-const popbtn = document.querySelectorAll('.btn-popup');
-popbtn.forEach((n) => {
-  n.addEventListener('click', () => {
-    const main = document.createElement('div');
-    main.className = 'main';
-    const desktoppopup = document.createElement('div');
-    desktoppopup.className = 'popup';
-    main.innerHTML = `
-    <div class="popupclose-btn" id="popupclose-btn">
-    <button class="close" >
-     <i class=" fa fa-times"></i>
-     </button>
-     </div>
-                <div>
-                  <img src="./images/desktop popup.png" alt="pop-up window image"
-                   class="popupcard-image">
-                </div>
-                <div class="title">
-                <h2 class="popupcard-title">
-                   ${popupH2[0][0]}
-                     <br>
-                      ${popupH2[0][1]}
-                </h2>
-                <div class="titleright">
-                 <a href="">
-                <button class="popupbtns" type="button">
-                    See Live<img src="./images/seelive.png" alt="live icon">
-                </button>
-                </a>
-                <a href="">
-                <button class="popupbtns" type="button">
-                  See Source <img src="./images/white-github.png" alt="github icon">
-                </button>
-                </a>
-                </div>
-                </div>
-                <ul class="popupcard-tags">
-                    <li>${popupTags[0]}</li>
-                    <li>${popupTags[1]}</li>
-                    <li>${popupTags[2]}</li>
-                    <li>${popupTags[3]}</li>
-                    <li>${popupTags[4]}</li>
-                    <li>${popupTags[5]}</li>
-                </ul>
-                <h6 class="card-text">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
-                </h6>
-              `;
-    main.appendChild(desktoppopup);
-    works.appendChild(main);
-    const close = document.querySelector('.close');
-    close.addEventListener('click', () => {
-      main.classList.remove('.popup');
-    });
-  });
-});*/
