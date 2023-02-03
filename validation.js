881fed05b2ee3d4756b92e8589cf4690853144e7
alexiscyber14 <142sledgehammer14@gmail.com>
2023-02-03 10:56:27 +0100

    Add email feild validation

--- validation.js
+++ validation.js
@@ -1,21 +1,7 @@
-// const form = document.querySelector('form');
-// const name = document.getElementById('name');
-// const firstName = document.getElementById('first-name');
-// const lastName = document.getElementById('last-name');
-// const email = document.getElementById('.email');
-// const textArea = document.getElementById('text-area');
-// const errorMessage = document.getElementById('error-message');
+const form = document.querySelector('form');
+const emailInput = document.querySelector("input[name='email']");
+const errorMessage = document.querySelector('.error-message');
 
-// function onSubmit() {
-//   const errors = [];
-
-//   if (email.value.trim() === ' ') {
-//     errors.push('email required');
-//   }
-
-//   if (errors.length > 0) {
-//     e.preventDefault();
-//     errorMessage.toggleAttribute('hidden');
-//     errorMessage.innerHTML = errors.join(', ');
-//   }
-// }
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailValue = emailInput.value;
