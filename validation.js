487d1d840db3129d79bd7c3c9a7f5ffc0d1c0256
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-02-03 12:52:29 +0300

    added validation branch

---
+++ validation.js
@@ -0,0 +1,21 @@
+// const form = document.querySelector('form');
+// const name = document.getElementById('name');
+// const firstName = document.getElementById('first-name');
+// const lastName = document.getElementById('last-name');
+// const email = document.getElementById('.email');
+// const textArea = document.getElementById('text-area');
+// const errorMessage = document.getElementById('error-message');
+
+// function onSubmit() {
+//   const errors = [];
+
+//   if (email.value.trim() === ' ') {
+//     errors.push('email required');
+//   }
+
+//   if (errors.length > 0) {
+//     e.preventDefault();
+//     errorMessage.toggleAttribute('hidden');
+//     errorMessage.innerHTML = errors.join(', ');
+//   }
+// }
